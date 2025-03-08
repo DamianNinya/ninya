b16ccb537e9af3f2048c201f1c9557cec33bcd2f
Damian <damianwoolfenden008@gmail.com>
2025-03-08 19:52:54 +0000

    Fixed some issues

--- form.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const submitButton = document.getElementById("submitAAR");
-    const formElements = document.querySelectorAll("#aar-form input, #aar-form textarea");
-    const maintenanceMessage = document.createElement("div");
-
-});
